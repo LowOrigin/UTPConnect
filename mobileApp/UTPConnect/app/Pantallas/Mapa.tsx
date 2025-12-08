5ca86d67a63f06eef5a0afae001c25407c0d5341
JeanSolano <solano.jean2003@gmail.com>
2025-12-08 14:59:55 -0500

    feat: configurar mapa básico con react-native-maps

--- mobileApp/UTPConnect/app/Pantallas/Mapa.tsx
+++ mobileApp/UTPConnect/app/Pantallas/Mapa.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
 
 export default function Mapa() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Esta seria la pantalla del mapa</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider="google"
+        initialRegion={{
+          latitude: 9.0245,
+          longitude: -79.531,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 9.0245,
+            longitude: -79.531,
+          }}
+          title="UTP"
+          description="Universidad Tecnológica de Panamá"
+        />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
